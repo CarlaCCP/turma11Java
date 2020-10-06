1e79e5b9f09a7eab9c019858d490c566b85e5f62
Carla Cristina <carldepaula230@gmail.com>
2020-10-06 08:10:32 -0300

    wip

---
+++ Java/aulas/src/Programa.java
@@ -0,0 +1,13 @@
+
+public class Programa {
+
+	public static void main(String[] args) {
+		int valor = 10;
+		String nome = "Ed";
+				System.out.println("Hello world");
+				System.out.println("Até AMANHÃ !!!");
+				System.out.println((valor + 10) + "\n");
+				System.out.println(nome+"\n");
+	}
+
+}
