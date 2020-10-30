42d903dec0a17a260a3461665f8c6d635af325e6
Carla Cristina <carldepaula230@gmail.com>
2020-10-30 10:54:42 -0300

    primeira aplicacao spring boot

---
+++ Spring Boot/workspace-sts-3.9.12.RELEASE/primeiroAplicacao/src/main/java/com/example/demo/PrimeiroAplicacaoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeiroAplicacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimeiroAplicacaoApplication.class, args);
+	}
+
+}
